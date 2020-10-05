43b407fcea654bde458f739aa94e305ddc5f7f76
Huy <huyduong.tran9@gmail.com>
2020-10-05 13:33:53 -0700

    Added scraper for Seeking Alpha. Need to work on time posted if have time

--- investing_news_aggregator/investing_news_aggregator/spiders/seeking_alpha.py
+++ investing_news_aggregator/investing_news_aggregator/spiders/seeking_alpha.py
@@ -1,27 +1,40 @@
 import scrapy
+import urllib.parse
+from datetime import datetime, timedelta
+from pytz import timezone
+
+from ..items import Article
 
 
 class SeekingAlphaSpider(scrapy.Spider):
     name = 'seekingalpha'
     allowed_domains = ['seekingalpha.com']
     start_urls = ['https://seekingalpha.com/market-news/']
+    today = datetime.now()
+    yesterday = today + timedelta(days=-1)
+    my_timezone = timezone('US/Pacific')
 
-    # filename = name + '_' + datetime.now().date().strftime("%m%d%Y")
-    # custom_settings = {"FEEDS": {f"results/{filename}.jl": {"format":"jl"}}}
+    filename = name + '_' + today.date().strftime("%m%d%Y")
+    custom_settings = {"FEEDS": {f"results/{filename}.jl": {"format":"jl"}}}
 
     def parse(self, response):
         item = Article()
         headlines = response.xpath('//li[@class="item"]')
         for headline in headlines:
-            temp = headline.xpath('div[@class="row"]/div[@class="col-md-8"]/div')
-            item['title'] = temp.xpath('h3/a/text()').get()
-            item['summary'] = temp.xpath('p/text()').get()
-            item['url'] = temp.xpath('h3/a/@href').get()
-            item['date_posted'] = datetime.strptime(temp.xpath('div/div/ul/li[@class="bon-car-meta-date"]/text()').get(), '%B %d, %Y')
+            item['title'] = headline.xpath('h4/a/text()').get()
+            item['url'] = urllib.parse.urljoin(f'https://www.{self.allowed_domains[0]}', headline.xpath('h4/a/@href').get())
+            date_posted = headline.xpath('div[@class="share-line"]/span[@class="item-date"]/text()').get()
+            if 'Today' in date_posted:
+                date = self.my_timezone.localize(self.today)
+                date = date.astimezone(self.my_timezone)
+                item['date_posted'] = date
+            elif 'Yesterday' in date_posted:
+                date = self.my_timezone.localize(self.yesterday)
+                date = date.astimezone(self.my_timezone)
+                item['date_posted'] = date
             item['date_extracted'] = datetime.now()
-            item['thumbnail_url'] = headline.xpath('div[@class="row"]/div[@class="col-md-4 newsListingImageContainer"]/a/img/@src').get()
             yield item
 
-        next_url = response.xpath('//div[@class="col-lg-2 col-sm-6 pagenumbers-next-container"]/a/@href').get()
+        next_url = response.xpath('//li[@class="next"]/a/@href').get()
         if next_url is not None:
             yield response.follow(next_url, callback=self.parse)
